0dbae79ed48701135e242e051cc41b9bc693057f
Bonnie Owens <arlolocos@gmail.com>
2026-03-04 00:08:42 -0500

    Build out server

--- server.ts
+++ server.ts
@@ -1,38 +1,44 @@
 import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import morgan from "morgan";
 import chalk from "chalk";
+import qs from 'qs';
 
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT;
 const ENV = process.env.NODE_ENV;
 
-// Connect to Database
-
 // Import Routes
-import routeOne from './Routes/routeOne';
-import routeTwo from './Routes/routeTwo';
-import routeThree from './Routes/routeThree';
+import resources from './routes/resources';
+
+// Import Middleware
+import errorHandler from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
+app.set('query parser', (str: string) => qs.parse(str));
 
-// Middleware
+// Dev Logging
 if (ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Set Security headers
+app.use(helmet());
+
 // Mount Routes
-app.use('/api/v1/{resources}', routeOne);
-app.use('/api/v1/{resources}', routeTwo);
-// app.use('/api/v1/{resources}', routeThree);
+app.use('/api/v1/resources', resources);
+
+// Error Handling Middleware
+app.use(errorHandler);
 
 const server = app.listen(PORT, () =>
   console.log(chalk.magenta.bold(`Server running in ${ENV} on port ${PORT}`)),
 );
 
-// Catch promise rejections
+// Catch Promise Rejections
 process.on("unhandledRejection", (err: Error, promise: Promise<any>) => {
   console.log(chalk.red(`Error: ${err.message}`));
   console.log(`${err.stack}`);
