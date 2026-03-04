1d4027f1747ac998a667de4851c385b5f7985739
Bonnie Owens <arlolocos@gmail.com>
2026-03-04 00:10:33 -0500

    Stub validation

---
+++ validation/resources.ts
@@ -0,0 +1,17 @@
+import { body, param } from "express-validator";
+
+export const validateCreateResource = [
+
+]
+
+export const validateUpdateResource = [
+
+]
+
+export const validateGetResource = [
+
+]
+
+export const validateDeleteResource = [
+
+]
