088f3f90fd6ea691d60c3396ae40a89a4a6f9186
Bonnie Owens <arlolocos@gmail.com>
2026-03-05 13:16:02 -0500

    Update prisma client

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '../generated/prisma/client';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({
-  adapter,
+  adapter
   // log: ["query", "info", "error"]
 });
 
